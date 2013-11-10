08ce3d2ddede5f3a9f0b3706506d898b953c379f
Takashi Yokoyama <tksyokoyama@gmail.com>
2013-11-10 19:38:21 +0900

    Step7の派生。time.Tickとtime.Afterを同時に使った時に、Afterの応答が得られないコード

--- sample.com/goroutines/main.go
+++ sample.com/goroutines/main.go
@@ -8,27 +8,28 @@ import (
 func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-	ch3 := make(chan int)
 
 	go process1(ch1)
 	go process2(ch2)
-	go process3(ch3)
 
 	// 複数Channelを待ち受ける場合（全て受け取るパターン）
-L:
+	// time.Tickとtime.Afterを同時に待ち受けると永久にAfterの応答がない。（受け取れない？）
+	ch3 := time.Tick(1 * time.Second)
 	for {
 		select {
 			case res1 := <-ch1:
 				fmt.Printf("process1 Finished[%d]\n", res1)
 			case res2 := <-ch2:
 				fmt.Printf("process2 Finished[%d]\n", res2)
-			case <-ch3:
+			case <- time.After(5 * time.Second):
+				// timeパッケージを使ったタイムアウト（この方式にすると、goroutineの順番がずれる？）
 				fmt.Println("Finish!")
-				break L
+				return
+			case <-ch3:
+				fmt.Printf("time.Tick\n")
 		}
 	}
 
-fmt.Println("break!")
 	// Output:
 	// process1: [01]・・・が大量に。
 	// process2: [01] Finished.・・・が大量に。
@@ -51,9 +52,4 @@ func process2(ch chan int) {
 	}
 
 	ch <- 2
-}
-
-func process3(ch chan int) {
-	time.Sleep(5000000000)
-	ch <- 3
 }
