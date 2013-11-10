f11382c3a088e28d96825f0a3663c593208e8148
Takashi Yokoyama <tksyokoyama@gmail.com>
2013-11-10 15:43:11 +0900

    Golang Cafe #3: Step4 呼び出し関数での待ち受け方

--- sample.com/goroutines/main.go
+++ sample.com/goroutines/main.go
@@ -7,13 +7,15 @@ import (
 func main() {
 	ch := make(chan int)
 
-	// goroutineの使い方（その1）
-	go func(chan int) {
-		fmt.Println("Goroutineの処理")
-		ch<- 1				// Channelに結果(1)を送る。
-	}(ch)
+	// goroutineの使い方（その2）
+	go useGoroutine(ch)
 
 	<-ch 					// Channelから結果(1)を受け取る。
 	// Output:
 	// Goroutineの処理
+}
+
+func useGoroutine(ch chan int) {
+	fmt.Println("Goroutineの処理")
+	ch <- 1 				// Channelに結果(1)を送る。
 }
