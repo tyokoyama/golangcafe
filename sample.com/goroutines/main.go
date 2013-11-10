7551f635f7b3fa87f39ad65f5113e6a481a2665c
Takashi Yokoyama <tksyokoyama@gmail.com>
2013-11-10 15:35:09 +0900

    Golang Cafe Step3: Channelでgoroutineの終了を待ち受ける

--- sample.com/goroutines/main.go
+++ sample.com/goroutines/main.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	// goroutineの使い方（その2）
-	go useGoroutine()
+	ch := make(chan int)
 
+	// goroutineの使い方（その1）
+	go func(chan int) {
+		fmt.Println("Goroutineの処理")
+		ch<- 1				// Channelに結果(1)を送る。
+	}(ch)
+
+	<-ch 					// Channelから結果(1)を受け取る。
 	// Output:
-	// 何も出ない。
-}
-
-func useGoroutine() {
-	fmt.Println("Goroutineの処理")
+	// Goroutineの処理
 }
