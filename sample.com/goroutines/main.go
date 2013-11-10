f78efa65c9425e1e48e1d86e58b96d435e8b03d8
Takashi Yokoyama <tksyokoyama@gmail.com>
2013-11-10 16:01:27 +0900

    Golang Cafe #3 Step5: バッファリングしたChannelの使い方

--- sample.com/goroutines/main.go
+++ sample.com/goroutines/main.go
@@ -5,17 +5,29 @@ import (
 )
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan int, 10)				// Channelに長さを指定するとバッファリングできるようになる。
 
-	// goroutineの使い方（その2）
-	go useGoroutine(ch)
+	// goroutineの使い方（その2）を（10個）大量に呼び出す。
+	for i := 1; i < 11; i++ {
+		go useGoroutine(i, ch)
+	}
 
-	<-ch 					// Channelから結果(1)を受け取る。
+	for no := range ch {					// Channelから結果(no)を受け取る。
+		fmt.Printf("goroutine[%02d] Finished.\n", no)
+	}
+
 	// Output:
-	// Goroutineの処理
+	// Goroutineの処理[01]・・・が大量に。
+	// goroutine[01] Finished.・・・が大量に。
 }
 
-func useGoroutine(ch chan int) {
-	fmt.Println("Goroutineの処理")
-	ch <- 1 				// Channelに結果(1)を送る。
+func useGoroutine(no int, ch chan int) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Goroutineの処理[%02d]\n", no)
+	}
+
+	ch <- no 				// Channelに結果(no)を送る。
+	if no == 10 {			// バッファリング後、これ以上データを送ることがない場合は、明示的にクローズする必要がある。
+		close(ch)
+	}
 }
