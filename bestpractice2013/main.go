974294cf2748082ef5660c46d146f6abf5935ab3
Takashi Yokoyama <tksyokoyama@gmail.com>
2014-03-02 19:13:46 +0900

    Golang Cafe #19 その３

--- bestpractice2013/main.go
+++ bestpractice2013/main.go
@@ -18,21 +18,29 @@ type binWriter struct {
     err  error
 }
 
-// Write writes a value to the provided writer in little endian form.
 func (w *binWriter) Write(v interface{}) {
     if w.err != nil {
         return
     }
-    if w.err = binary.Write(w.w, binary.LittleEndian, v); w.err == nil {
-        w.size += int64(binary.Size(v))
+    switch v.(type) {
+    case string:
+        s := v.(string)
+        w.Write(int32(len(s)))
+        w.Write([]byte(s))
+    case int:
+        i := v.(int)
+        w.Write(int64(i))
+    default:
+        if w.err = binary.Write(w.w, binary.LittleEndian, v); w.err == nil {
+            w.size += int64(binary.Size(v))
+        }
     }
 }
 
 func (g *Gopher) WriteTo(w io.Writer) (size int64, err error) {
     bw := &binWriter{w: w}
-    bw.Write(int32(len(g.Name)))
-    bw.Write([]byte(g.Name))
-    bw.Write(int64(g.AgeYears))
+    bw.Write(g.Name)
+    bw.Write(g.AgeYears)
 
     return bw.size, bw.err
 }
