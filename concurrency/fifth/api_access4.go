a2597093a56de521de70aad5f8ed13e2b65f86d4
Takashi Yokoyama <tksyokoyama@gmail.com>
2019-11-24 19:40:10 +0900

    5章5.6のソースコードを追加

---
+++ concurrency/fifth/api_access4.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func(l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
+func Per(eventCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration / time.Duration(eventCount))
+}
+
+func Open() *APIConnection {
+	return &APIConnection {
+		apiLimit: MultiLimiter (
+			rate.NewLimiter(Per(2, time.Second), 2),
+			rate.NewLimiter(Per(10, time.Minute), 10),
+		),
+		diskLimit: MultiLimiter (
+			rate.NewLimiter(rate.Limit(1), 1),
+		),
+		networkLimit: MultiLimiter (
+			rate.NewLimiter(Per(3, time.Second), 3),
+		),
+	}
+}
+
+type APIConnection struct {
+	networkLimit,
+	diskLimit,
+	apiLimit RateLimiter
+}
+
+func (a *APIConnection) ReadFile(ctx context.Context) error {
+	err := MultiLimiter(a.apiLimit, a.diskLimit).Wait(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	err := MultiLimiter(a.apiLimit, a.diskLimit).Wait(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	defer log.Printf("Done.")
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	apiConnection := Open()
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ReadFile(context.Background())
+			if err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Printf("ReadFile")
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ResolveAddress(context.Background())
+			if err != nil {
+				log.Printf("cannot ResolveAddress: %v", err)
+			}
+			log.Printf("ResolveAddress")
+		}()
+	}
+
+	wg.Wait()
+}
